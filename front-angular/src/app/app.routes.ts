f0eca9c279cc2c548a70997804410444400b000a
laisdev <laiscc575@gmail.com>
2024-12-24 16:54:48 -0300

    :construction: feat: includind routes in the file 'app.routes.ts'

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { EmployeeAddComponent } from './employee-add/employee-add.component'
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component'
+import { EmployeeGetComponent } from './employee-get/employee-get.component'
+
+export const routes: Routes = [
+  { path: 'employee/create', component: EmployeeAddComponent }, //localhost:4200/employee/create
+  { path: 'employee/:id', component: EmployeeEditComponent },
+  { path: 'employee', component: EmployeeGetComponent }
+];
