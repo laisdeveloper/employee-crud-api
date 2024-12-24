0eb8e9f2ce53010b24fcf8cff321394af27621d2
laisdev <laiscc575@gmail.com>
2024-12-24 16:41:00 -0300

    :construction: feat: creating components of add, edit and get

---
+++ front-angular/src/app/employee-get/employee-get.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-get',
+  imports: [],
+  templateUrl: './employee-get.component.html',
+  styleUrl: './employee-get.component.css'
+})
+export class EmployeeGetComponent {
+
+}
