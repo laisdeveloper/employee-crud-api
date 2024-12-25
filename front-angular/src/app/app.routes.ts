9615da466a0f8628a5536b96db0ef9c725409978
laisdev <laiscc575@gmail.com>
2024-12-25 17:13:34 -0300

    :construction: fix: resolving bug of import dependencies in NgModule...

--- front-angular/src/app/app.routes.ts
+++ front-angular/src/app/app.routes.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-import { EmployeeAddComponent } from './employee-add/employee-add.component'
-import { EmployeeEditComponent } from './employee-edit/employee-edit.component'
-import { EmployeeGetComponent } from './employee-get/employee-get.component'
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeGetComponent } from './employee-get/employee-get.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  //{ path: '', redirectTo: '/employees', pathMatch: 'full'},
-  { path: '', component: AppComponent },
+  { path: '', component: EmployeeGetComponent }, // Página inicial (ajustado para um componente de lista)
   { path: 'employee/create', component: EmployeeAddComponent },
   { path: 'employees/:id', component: EmployeeEditComponent },
   { path: 'employees', component: EmployeeGetComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
+  ],
+  exports: [RouterModule
+  ],
+  declarations: [] // Não precisa declarar os componentes standalone aqui
 })
 export class AppRoutingModule { }
--- front-angular/src/app/employee-add/employee-add.component.ts
+++ front-angular/src/app/employee-add/employee-add.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserPlus, faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-add',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FontAwesomeModule ],
   templateUrl: './employee-add.component.html',
   styleUrl: './employee-add.component.css'
 })
-export class EmployeeAddComponent {
 
+export class EmployeeAddComponent implements OnInit {
+
+  icons = {
+    faUserPlus,
+    faUserEdit
+  };
+
+  employeeForm: FormGroup;
+  employee_id?: number; // Declarando como opcional
+
+  constructor(private formBuilder: FormBuilder) {
+    this.employeeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      job_role: ['', Validators.required],
+      salary: ['', Validators.required],
+      birth: ['', Validators.required],
+      employee_registration: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.checkParam();
+  }
+
+  checkParam() {
+
+  }
+
+  onSubmit() {
+    if (!this.employee_id) {
+      console.log('Create new employee');
+      // Lógica para criar novo empregado
+    } else {
+      console.log('Update employee');
+      // Lógica para atualizar empregado
+    }
+  }
 }
